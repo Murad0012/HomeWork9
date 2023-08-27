1fc2a9dea8645e01447196ac0a079827c7df7983
Predator <murad.mammedzade11@gmail.com>
2023-08-27 06:55:20 -0700

    save

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "CodeAcademy is a good place to study";
+            var res2 = input.Split(' ');
+            int count = 0;
+            foreach (var item in res2)
+            {
+                count++;
+            }
+            Console.WriteLine("Total number of words in the string is : " + count);
+        }
+    }
+}
